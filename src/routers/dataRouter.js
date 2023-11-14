0a338293d59741fe25576f22c2c48f53b48198c7
sunwoo <sunrain1223@gmail.com>
2023-11-14 18:35:21 +0900

    fix

--- src/routers/dataRouter.js
+++ src/routers/dataRouter.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import {createUser} from "../services/userService.js";
-import {fetchLastTrainTime} from "../services/dataService.js";
+import {fetchLastTrainTime, getBusInfo} from "../services/dataService.js";
 
 const router = Router();
 
@@ -16,4 +16,96 @@ router.get('/', async (req, res) => {
     }
 });
 
+// 장소 생성 엔드포인트
+router.post('/places', async (req, res) => {
+    try {
+        const { name, address } = req.body;
+
+        // Firestore에 데이터 추가
+        const docRef = await firestore.collection('places').add({
+            name,
+            address
+        });
+
+        res.status(201).json({ id: docRef.id, message: 'Place added successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/places', async (req, res) => {
+    try {
+        // Firestore에서 모든 장소 데이터 가져오기
+        const placesSnapshot = await firestore.collection('places').get();
+        const places = placesSnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+
+        res.status(200).json(places);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// 특정 장소 읽기 엔드포인트
+router.get('/places/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Firestore에서 특정 장소 데이터 가져오기
+        const docRef = firestore.collection('places').doc(id);
+        const doc = await docRef.get();
+
+        if (!doc.exists) {
+            res.status(404).json({ error: 'Place not found' });
+        } else {
+            res.status(200).json({ id: doc.id, ...doc.data() });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// 장소 갱신 엔드포인트
+router.put('/places/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, address } = req.body;
+
+        // Firestore에서 특정 장소 데이터 갱신
+        const docRef = firestore.collection('places').doc(id);
+        await docRef.update({ name, address });
+
+        res.status(200).json({ id, message: 'Place updated successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+// 장소 삭제 엔드포인트
+router.delete('/places/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        // Firestore에서 특정 장소 데이터 삭제
+        const docRef = firestore.collection('places').doc(id);
+        await docRef.delete();
+
+        res.status(200).json({ id, message: 'Place deleted successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+router.get('/busInfo', async (req, res) => {
+    try {
+        const response = await getBusInfo('12121');
+        res.status(200).json({ success: true });
+    } catch (e) {
+        console.log(e);
+    }
+})
 export default router;
--- src/services/dataService.js
+++ src/services/dataService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+const serviceKey = 'w8c+i8IxhUxkWN9tRre/aI/saBtqwIKe7zEjK+jhcvf/KivE8+RkxWsIn5FaS1pHdQNSLoxd5z8ISoncJddfPw==';
 // code: 역 코드
 // day: 평일, 주말
 // direction: 방향이 어디인지
@@ -34,3 +34,19 @@ export async function getArrInfoByRouteList(busId, stId, ord) {
         throw error;
     }
 }
+
+export async function getBusInfo(arsId) {
+    try {
+        let url = 'http://ws.bus.go.kr/api/rest/stationinfo/getRouteByStation'; /*URL*/
+        let queryParams = '?' + encodeURIComponent('serviceKey') + '='+ process.env.SERVICEKEY; /*Service Key*/
+        queryParams += '&' + encodeURIComponent('stSrch') + '=' + encodeURIComponent('12121'); /**/
+        // const apiUrl = `http://ws.bus.go.kr/api/rest/stationinfo/getRouteByStation?ServiceKey=${process.env.SERVICEKEY}&arsId=${arsId}&resultType=json`
+        // console.log(apiUrl);
+        const response = await axios.get(url + queryParams);
+        console.log(response);
+        return response.data;
+    } catch (e) {
+        console.log(e);
+    }
+}
+
